70623a62d3bd4b329bf604e3b7482901cd81901f
League Student <league@iMac-20.attlocal.net>
2020-02-02 14:24:53 -0800

    attempt screenshotting and downloading

--- selenium/src/selenium/tester.java
+++ selenium/src/selenium/tester.java
@@ -1,27 +1,33 @@
 package selenium;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-//comment the above line and uncomment below line to use Chrome
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.io.Files;
 public class tester {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","/Users/league/Desktop/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		
-		String baseURL = "http://demo.guru99.com/test/newtours/";
+		String baseURL = "https://www.dmv.ca.gov/portal/dmv/detail/forms/reg/reg138";
 		
 		driver.get(baseURL);
-		WebElement security = driver.findElement(By.xpath("//*[@id=\"navbar-brand-centered\"]/ul/li[6]/a"));
-		int x = security.getLocation().getX();
-		int y = security.getLocation().getY();
-		System.out.println(x);
-		System.out.println(y);
-		Actions acts = new Actions(driver);
-		acts.moveByOffset(x + 5, y + 5).click();
-		//driver.close();
+		WebElement pdfLink = driver.findElement(By.xpath("//*[@id=\"app_content\"]/p[5]/a"));
+		pdfLink.click();
+		driver.findElement(By.xpath("//*[@id=\"icon\"]")).click();
+		driver.close();
 	}
 }
